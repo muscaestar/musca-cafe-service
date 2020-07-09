5d5775f9db6c714e72a49c8d1028b7ccfde8d180
musca <muscaestar@hotmail.com>
2020-07-10 02:32:26 +1000

    initial commit

---
+++ src/main/java/xyz/muscaestar/muscacafeservice/MuscaCafeServiceApplication.java
@@ -0,0 +1,13 @@
+package xyz.muscaestar.muscacafeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuscaCafeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MuscaCafeServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/muscaestar/muscacafeservice/MuscaCafeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.muscaestar.muscacafeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuscaCafeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
